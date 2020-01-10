39e5c4c37a1be9cde787d75b4d1cbbb8e52d4d4a
Catherine Pham <crypticwoodwhite@gmail.com>
2020-01-09 17:50:02 -0800

    env

---
+++ config/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "development": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "Jul@1989",
+    "database": "mediger",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "username": process.env.JAWSDBUSER,
+    "password": process.env.JAWSDBPW,
+    "database": process.env.JAWSDBDB,
+    "host": process.env.JAWSDBHOST,
+    "port": 3306,
+    "dialect": "mysql"
+  }
+}

7512f247e6dff8292d3708365c6fd3e84391cd06
Catherine Pham <crypticwoodwhite@gmail.com>
2020-01-27 21:01:15 -0800

    config

--- client/src/auth_config.js
+++ client/src/auth_config.js
@@ -1,9 +1,6 @@
-require("dotenv").config();
-
-const config = {
-  domain: process.env.AUTH0DOMAIN,
-  clientId: process.env.AUTH0ID,
-  cientSecret: process.env.AUTH0SECRET,
-  audience: process.env.AUTH0AUDIENCE
+module.exports = {
+  "domain": process.env.AUTH0DOMAIN,
+  "clientId": process.env.AUTH0ID,
+  "cientSecret": process.env.AUTH0SECRET,
+  "audience": process.env.AUTH0AUDIENCE
 }
-export default config;
